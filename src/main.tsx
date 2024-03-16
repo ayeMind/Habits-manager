805a1db8a0a07fb2148cc1af1cb120391e441dff
Yan <fzerix@gmail.com>
2024-03-16 15:19:05 +0300

    add page 404

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,10 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
+import { MantineProvider } from '@mantine/core';
+
 import 'app/global.css';
+import '@mantine/core/styles.css';
 
 import Home from 'pages/home/Home.tsx';
 import NotFound from 'pages/not-found/NotFound';
@@ -16,14 +19,12 @@ const router = createBrowserRouter([
     element: <Home />,
     errorElement: <NotFound />,
   },
-  {
-    path: "about",
-    element: <div>About</div>,
-  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <MantineProvider>
+      <RouterProvider router={router} />
+    </MantineProvider>
   </React.StrictMode>,
 )
--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -1,10 +1,16 @@
+import classes from './styles.module.css';
+import { Button } from '@mantine/core';
+import { Link } from 'react-router-dom';
+
 const NotFound = () => {
   return (
-    <>
-       <div>
-            Not Found
+       <div className={classes["page"]}>
+            <h1>Not Found</h1>
+            <h2 className={classes["error-code"]}>404</h2>
+            <Link to='/'>
+              <Button variant="outline">Go Home</Button>
+            </Link>
        </div>
-    </>
   );
 };
 
